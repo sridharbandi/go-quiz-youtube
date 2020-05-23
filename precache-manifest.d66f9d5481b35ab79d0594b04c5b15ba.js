3b6fe23b9965fe3dec6124b19c4602af4db4c7c5
Sridhar Bandi <sridhar.bandi.ece@gmail.com>
2020-05-23 21:05:21 +0200

    Updates

---
+++ precache-manifest.d66f9d5481b35ab79d0594b04c5b15ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86044fe7cfac61bb8dbb815fee02ac72",
+    "url": "/go-quiz-youtube/index.html"
+  },
+  {
+    "revision": "0477462ecde40c38a056",
+    "url": "/go-quiz-youtube/static/css/main.61918a17.chunk.css"
+  },
+  {
+    "revision": "1fd6f51e57b8e0fa21d8",
+    "url": "/go-quiz-youtube/static/js/2.5f4f0d0a.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/go-quiz-youtube/static/js/2.5f4f0d0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0477462ecde40c38a056",
+    "url": "/go-quiz-youtube/static/js/main.ba2fdef3.chunk.js"
+  },
+  {
+    "revision": "464034b88a91fb8cfe6e",
+    "url": "/go-quiz-youtube/static/js/runtime-main.cad728c9.js"
+  }
+]);
